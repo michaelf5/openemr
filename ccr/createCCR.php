79395600bbfdcd31d49a74ca0fe009f256fc4214
Michael Firriolo <mfirriolo@gshsys.com>
2011-01-02 13:39:22 -0500

    Updated Header section

--- ccr/createCCR.php
+++ ccr/createCCR.php
@@ -40,9 +40,6 @@ function createCCR($action){
 	
 	echo '<!--';
 
-	$result = getHeaderData();
-	while ($row = sqlFetchArray($result)) {
-
 	   $ccr = new DOMDocument('1.0','UTF-8');
 	   $e_styleSheet = $ccr->createProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="ccr.xsl"');
 	   $ccr->appendChild($e_styleSheet);
@@ -52,69 +49,7 @@ function createCCR($action){
 
 	   /////////////// Header
 
-	   $e_ccrDocObjID = $ccr->createElement('CCRDocumentObjectID', getUuid());
-	   $e_ccr->appendChild($e_ccrDocObjID);
-
-	   $e_Language = $ccr->createElement('Language', 'English');
-	   $e_ccr->appendChild($e_Language);
-
-	   $e_Version = $ccr->createElement('Version', 'V1.0');
-	   $e_ccr->appendChild($e_Version);
-
-	   $e_dateTime = $ccr->createElement('DateTime');
-	   $e_ccr->appendChild($e_dateTime);
-
-           $e_ExactDateTime = $ccr->createElement('ExactDateTime', date('Y-m-d\TH:i:s\Z'));
-	   $e_dateTime->appendChild($e_ExactDateTime);
-
-	   $e_patient = $ccr->createElement('Patient');
-	   $e_ccr->appendChild($e_patient);
-
-	   $e_ActorID = $ccr->createElement('ActorID', $row['patient_id']);
-	   $e_patient->appendChild($e_ActorID);
-	   
-	   //Header From:
-	   $e_From = $ccr->createElement('From');
-	   $e_ccr->appendChild($e_From);
-
-	   $e_ActorLink = $ccr->createElement('ActorLink');
-	   $e_From->appendChild($e_ActorLink);
-
-	   $e_ActorID = $ccr->createElement('ActorID', $authorID );
-	   $e_ActorLink->appendChild($e_ActorID);
-
-	   $e_ActorRole = $ccr->createElement('ActorRole');
-	   $e_ActorLink->appendChild($e_ActorRole);
-
-	   $e_Text = $ccr->createElement('Text', 'author');
-	   $e_ActorRole->appendChild($e_Text);
-	   
-	   //Header To:	
-	   $e_To = $ccr->createElement('To');
-	   $e_ccr->appendChild($e_To);
-
-           $e_ActorLink = $ccr->createElement('ActorLink');
-           $e_To->appendChild($e_ActorLink);
-
-           $e_ActorID = $ccr->createElement('ActorID', $row['patient_id']);
-           $e_ActorLink->appendChild($e_ActorID);
-
-           $e_ActorRole = $ccr->createElement('ActorRole');
-           $e_ActorLink->appendChild($e_ActorRole);
-
-           $e_Text = $ccr->createElement('Text', 'patient');
-           $e_ActorRole->appendChild($e_Text);
-
-	   //Header Purpose:
-	   $e_Purpose = $ccr->createElement('Purpose');
-	   $e_ccr->appendChild($e_Purpose);
-
-	   $e_Description = $ccr->createElement('Description');
-	   $e_Purpose->appendChild($e_Description);
-
-	   $e_Text = $ccr->createElement('Text', 'Summary of patient information');
-	   $e_Description->appendChild($e_Text);
-
+	   require_once("createCCRHeader.php");
 	   $e_Body = $ccr->createElement('Body');
 	   $e_ccr->appendChild($e_Body);
 	   
